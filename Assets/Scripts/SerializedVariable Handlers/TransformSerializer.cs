37f3fbe32168d13d875981df6518895a722baf42
Jirushi_I <jirushi.insignia@outlook.com>
2020-04-18 14:33:46 -0700

    Player and map

---
+++ Assets/Scripts/SerializedVariable Handlers/TransformSerializer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using ScriptableObjects.SerializedVariables;
+using UnityEngine;
+
+public class TransformSerializer : MonoBehaviour
+{
+    [SerializeField] private SerializedVector positionVector;
+    [SerializeField] private SerializedVector rotationEuler;
+    [SerializeField] private SerializedVector scaleVector;
+
+    private void Update()
+    {
+        SetVectorIfExists(positionVector, transform.position);
+        SetVectorIfExists(rotationEuler, transform.eulerAngles);
+        SetVectorIfExists(scaleVector, transform.localScale);
+    }
+
+    private void SetVectorIfExists(SerializedVector serializedVector, Vector3 value)
+    {
+        if (serializedVector)
+            serializedVector.value = value;
+    }
+}
